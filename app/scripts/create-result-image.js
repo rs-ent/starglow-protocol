3783263bbdae33f747f7f47ad210727adb8efa39
rs-ent <rs.ent.contact@gmail.com>
2025-01-28 17:43:50 +0900

    servelse

--- app/scripts/create-result-image.js
+++ app/scripts/create-result-image.js
@@ -1,33 +1,16 @@
 import sharp from "sharp";
 import fs from "fs/promises";
+import puppeteer from "puppeteer-core";
+import chromium from "@sparticuz/chromium";
 
 export async function createResultImage(pollId) {
-
-    let puppeteerImport, chromiumImport;
-    if (process.env.NEXT_PUBLIC_BASE_URL === "http://localhost:3001" || process.env.NEXT_PUBLIC_BASE_URL === "http://localhost:3000") {
-        // 개발환경
-        puppeteerImport = await import("puppeteer");
-        chromiumImport = null;
-    } else {
-        // 프로덕션(배포)환경
-        puppeteerImport = await import("puppeteer-core");
-        chromiumImport = await import("@sparticuz/chromium");
-    }
-
-    const puppeteer = puppeteerImport.default;
-    const chromium = chromiumImport?.default;
-    
-    const executablePath = chromium
-        ? await chromium.executablePath
-        : undefined;
-
-    const args = chromium ? chromium.args : [];
+    const executablePath = await chromium.executablePath;
     const browser = await puppeteer.launch({
-        headless: "new",
         executablePath,
-        args,
-    });
-    
+        headless: "new",
+        args: chromium.args,
+      });
+      
     const page = await browser.newPage();
     await page.setViewport({ width: 1300, height: 1080, deviceScaleFactor: 1.8 });
 
