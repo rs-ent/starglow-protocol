ec1946e8c5316ddc22eac4fe07baf0ccaa6c9228
rs-ent <rs.ent.contact@gmail.com>
2025-02-10 16:20:02 +0900

    Telegram Log2

--- app/telegram/login/page.jsx
+++ app/telegram/login/page.jsx
@@ -2,34 +2,37 @@
 
 import React, { useEffect } from "react";
 import Script from "next/script";
-import { useRouter } from "next/navigation";
 
 export default function TelegramLoginPage() {
-  const router = useRouter();
-
-  // Telegram 로그인 콜백 설정
+  // 글로벌 함수로 onTelegramAuth를 설정합니다.
   useEffect(() => {
     window.onTelegramAuth = (user) => {
-      router.push("/");
+      alert(
+        'Logged in as ' +
+          user.first_name +
+          ' ' +
+          user.last_name +
+          ' (' +
+          user.id +
+          (user.username ? ', @' + user.username : '') +
+          ')'
+      );
     };
-  }, [router]);
+  }, []);
 
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-black">
+    <div className="min-h-screen flex flex-col items-center justify-center bg-black text-white">
       <h1 className="text-3xl font-bold mb-4">Telegram Login</h1>
       <p className="mb-8">Please login with your Telegram account to continue.</p>
-      
-      {/* Telegram 로그인 위젯 스크립트 */}
       <Script
         id="telegram-login-script"
         strategy="afterInteractive"
-        src="https://telegram.org/js/telegram-widget.js?15"
+        src="https://telegram.org/js/telegram-widget.js?22"
         data-telegram-login="starglow_redslippers_bot"
-        data-size="large"
+        data-size="medium"
         data-userpic="false"
         data-request-access="write"
-        data-callback="onTelegramAuth"
-        data-lang="en"
+        data-onauth="onTelegramAuth(user)"
       />
     </div>
   );
