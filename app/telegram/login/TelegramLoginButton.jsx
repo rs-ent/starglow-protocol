e8d835751f80f4c0d6e1d4c1bea179c1346fbf98
WaydCloud <goldencho3@gmail.com>
2025-02-11 13:13:56 +0900

    tlogin

--- app/telegram/login/TelegramLoginButton.jsx
+++ app/telegram/login/TelegramLoginButton.jsx
@@ -19,36 +19,40 @@ export default function TelegramLoginButton() {
   };
 
   useEffect(() => {
-    if (session?.user) return;
+    if (process.env.NEXTAUTH_URL === "http://localhost:3000") {
+      
+    } else {
+      if (session?.user) return;
 
-    window.onTelegramAuth = onTelegramAuth;
+      window.onTelegramAuth = onTelegramAuth;
 
-    const script = document.createElement("script");
-    script.src = "https://telegram.org/js/telegram-widget.js?22";
-    script.setAttribute("data-telegram-login", "starglow_redslippers_bot");
-    script.setAttribute("data-size", "large");
-    script.setAttribute("data-userpic", "true");
-    script.setAttribute("data-request-access", "write");
-    script.setAttribute("data-onauth", "onTelegramAuth(user)");
-    script.async = true;
+      const script = document.createElement("script");
+      script.src = "https://telegram.org/js/telegram-widget.js?22";
+      script.setAttribute("data-telegram-login", "starglow_redslippers_bot");
+      script.setAttribute("data-size", "large");
+      script.setAttribute("data-userpic", "true");
+      script.setAttribute("data-request-access", "write");
+      script.setAttribute("data-onauth", "onTelegramAuth(user)");
+      script.async = true;
 
-    if (containerRef.current) {
-      containerRef.current.appendChild(script);
-    }
-
-    return () => {
-      if (containerRef.current && containerRef.current.contains(script)) {
-        containerRef.current.removeChild(script);
+      if (containerRef.current) {
+        containerRef.current.appendChild(script);
       }
-      delete window.onTelegramAuth;
-    };
+
+      return () => {
+        if (containerRef.current && containerRef.current.contains(script)) {
+          containerRef.current.removeChild(script);
+        }
+        delete window.onTelegramAuth;
+      };
+    }
   }, []);
 
   const handlePopup = () => {
     setPopupOpened((prev) => !prev);
   };
 
-  const shouldRenderWidget = status !== "loading" && !session?.user;
+  const shouldRenderWidget = !session?.user;
 
   return session?.user ? (
     <div className="login-completed-box">
@@ -63,7 +67,7 @@ export default function TelegramLoginButton() {
           height={17}
           className="mr-2"
         />
-        Play STARGLOW
+        {session.user.username}
       </button>
 
       {popupOpened && (
