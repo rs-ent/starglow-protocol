eec4a600acdfa3d196f3762a9fefa254efe48563
WaydCloud <goldencho3@gmail.com>
2025-03-06 13:38:53 +0900

    start

---
+++ app/home/page.js
@@ -0,0 +1,15 @@
+"use client";
+
+import dynamic from 'next/dynamic';
+
+const Header = dynamic(() => import('../components/Header'), { ssr: false });
+const Body = dynamic(() => import('../components/Body'), { ssr: false });
+
+export default function Main() {
+    return (
+        <div className="bg-black overflow-x-clip">
+            <Header />
+            <Body />
+        </div>
+    );
+}
---
+++ app/start/page.js
@@ -0,0 +1,5 @@
+import { redirect } from "next/navigation";
+
+export default function MiniAppRouter() {
+    redirect("https://t.me/starglow_redslippers_bot?start=r_vko5zj");
+}
