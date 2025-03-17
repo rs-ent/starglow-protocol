82c817de9eaba7e5c7829a4996bbab408cc79a00
WaydCloud <goldencho3@gmail.com>
2025-03-17 13:54:37 +0900

    not store

--- app/nft-store/page.jsx
+++ app/nft-store/page.jsx
@@ -1,14 +1,12 @@
 /// app\nft-store\page.jsx
 
 import ZkLogin from "../components/zkLogin"
-import NFTStore from "./NFTStore"
 
 export default async function NFTStorePage() {
 
     return (
         <div className="bg-black">
             <ZkLogin />
-            <NFTStore />
         </div>
     )
 }
